9f9c8882358bb17e86ad4e85a20faec1e3ec28f7
gqqnbig <gqqnb2005@gmail.com>
2019-11-23 15:47:52 -0800

    输出更多提示信息

--- TreeMiner.cpp
+++ TreeMiner.cpp
@@ -2,19 +2,48 @@
 #include <cstdio>
 #include <cstring>
 #include <vector>
+
+#include <string>
+#include <iterator>
+#include <sstream>
+
+#define emptySet "[]"
+#define joinOperator "join"
+
 //#define debug
 using namespace std;
 typedef vector<int> vi;
 const int N = 100000 + 10;
 const int MinSup = 2;
+
+string vectorToString(vector<int> &m, char *empty) {
+	if (!m.empty()) {
+		std::ostringstream oss;
+		// Convert all but the last element to avoid a trailing ","
+		copy(m.begin(), m.end() - 1, ostream_iterator<int>(oss, ","));
+
+		// Now add the last element with no delimiter
+		oss << m.back();
+		return "[" + oss.str() + "]";
+	} else
+		return empty;
+}
+
 struct Pair {
-  int x, p;
+  int content, position;
   Pair() {}
   Pair(int _x, int _p) {
-	  x = _x;
-	  p = _p;
+	  content = _x;
+	  position = _p;
   }
 
+  string toString() {
+	  return "[" + to_string(content) + "," + to_string(position) + "]";
+  }
+
+  string toLabelString() {
+	  return "[" + string(1, 'A' - 1 + content) + "," + to_string(position) + "]";
+  }
 
 };
 struct Scope {
@@ -27,6 +56,10 @@ struct Scope {
 	  m = _m;
 	  m.push_back(lx);
 	  s = _s;
+  }
+
+  std::string toString() {
+	  return "(t=" + std::to_string(tid) + ", m=" + vectorToString(m, emptySet) + ", s=" + s.toString() + ")";
   }
 };
 struct Element {
@@ -48,6 +81,21 @@ struct Element {
 struct Subtree {
   vi prefix;
   vector<Element> elements;
+
+  string prefixToString() {
+	  string s;
+	  char buff[50];
+	  for (int i = 0; i < prefix.size(); i++) {
+		  if (prefix[i] == -1)
+			  sprintf(buff, "%d ", prefix[i]);
+		  else
+			  printf(buff, "%c ", 'A' - 1 + prefix[i]);
+
+		  s = s + buff;
+	  }
+	  return s;
+  }
+
   //print the tree
   void output() {
 
@@ -68,7 +116,7 @@ struct Subtree {
 		  // puts("----------");
 		  printf("New node: ");
 
-		  printf("%1$c %2$d (add node %1$c to DFS position %2$d)\n", 'A' + elements[i].label.x - 1, elements[i].label.p);
+		  printf("%1$c %2$d (add node %1$c to DFS position %2$d)\n", 'A' + elements[i].label.content - 1, elements[i].label.position);
 #ifdef debug
 		  vector<Scope> &scopes = elements[i].scopes;
 		  for (int j = 0; j < scopes.size(); j++) {
@@ -76,7 +124,7 @@ struct Subtree {
 			  for (int k = 0; k < scopes[j].m.size(); k++)
 				  printf("%d%c", scopes[j].m[k],
 						 k + 1 == scopes[j].m.size() ? ',' : ' ');
-			  printf("[%d,%d]\n", scopes[j].s.x, scopes[j].s.p);
+			  printf("[%d,%d]\n", scopes[j].s.content, scopes[j].s.position);
 		  }
 #endif
 	  }
@@ -103,14 +151,14 @@ void work(const vi &v, int tid) {
 			continue;
 		Scope scope;
 		scope.tid = tid;
-		scope.s.x = lx;
+		scope.s.content = lx;
 		for (int j = i + 1, tot = 1, ux = lx; j < v.size(); j++) {
 			if (v[j] == -1)
 				tot--;
 			else
 				tot++, ux++;
 			if (tot == 0) {
-				scope.s.p = ux;
+				scope.s.position = ux;
 				element[v[i]].label = Pair(v[i], -1);
 				element[v[i]].scopes.push_back(scope);
 				break;
@@ -124,7 +172,7 @@ void Prefix_Generate(const vi &pre1, const Pair &label, vi &pre2) {
 	for (int i = 0, fa = 0; i < pre1.size(); i++) {
 		if (pre1[i] == -1)
 			continue;
-		if (fa++ == label.p) {
+		if (fa++ == label.position) {
 			int cnt = 0;
 			for (int j = i + 1; j < pre1.size(); j++) {
 				if (pre1[j] != -1)
@@ -137,7 +185,7 @@ void Prefix_Generate(const vi &pre1, const Pair &label, vi &pre2) {
 			break;
 		}
 	}
-	pre2.push_back(label.x);
+	pre2.push_back(label.content);
 }
 void TMdfs(Subtree P) {
 	P.output();
@@ -150,37 +198,44 @@ void TMdfs(Subtree P) {
 			Pair py = P.elements[j].label;
 			vector<Scope> &scopey = P.elements[j].scopes;
 			// In-scope Test
-			if (px.p == py.p) {
+			if (px.position == py.position) {
 				Element eleIn;
-				eleIn.label = Pair(py.x, py.p + 1); //(y,j+1)
+				eleIn.label = Pair(py.content, py.position + 1); //(y,j+1)
+				printf("In-scope join %s and %s, get %s:\n", px.toLabelString().c_str(), py.toLabelString().c_str(), eleIn.label.toLabelString().c_str());
 				for (int r = 0; r < scopex.size(); r++)
 					for (int t = 0; t < scopey.size(); t++) {
+						//skip same element
 						if ((&scopex[r]) == (&scopey[t]))
 							continue;
-						if (scopex[r].tid == scopey[t].tid && scopex[r].m == scopey[t].m &&
-							scopex[r].s.x <= scopey[t].s.x &&
-							scopex[r].s.p >= scopey[t].s.p) {
-							eleIn.scopes.push_back(Scope(scopex[r].tid, scopex[r].m,
-														 scopex[r].s.x, scopey[t].s));
+						bool mtest = scopex[r].m == scopey[t].m;
+						if (scopex[r].tid == scopey[t].tid && mtest &&
+							scopex[r].s.content <= scopey[t].s.content &&
+							scopex[r].s.position >= scopey[t].s.position) {
+
+							Scope s = Scope(scopex[r].tid, scopex[r].m,
+											scopex[r].s.content, scopey[t].s);
+							printf("%s %s %s=%s.\n", scopex[r].toString().c_str(), joinOperator, scopey[t].toString().c_str(), s.toString().c_str());
+							eleIn.scopes.push_back(s);
 						}
 					}
-				if (eleIn.check())
+				if (eleIn.check()) {
 					PP.elements.push_back(eleIn);
-				else
-				{
-					printf("Given prefix xx with new node yy, the pattern is not frequent.");
+					puts("The pattern is frequent.");
+				} else {
+					puts("The pattern is not frequent.");
 				}
 			}
 			// Out-scope Test
-			if (py.p != -1) {
+			// That position is -1 means the node is the root.
+			if (py.position != -1) {
 				Element eleOut;
-				eleOut.label = Pair(py.x, py.p); //(y,j)
+				eleOut.label = Pair(py.content, py.position); //(y,j)
 				for (int r = 0; r < scopex.size(); r++)
 					for (int t = 0; t < scopey.size(); t++) {
 						if (scopex[r].tid == scopey[t].tid && scopex[r].m == scopey[t].m &&
-							scopex[r].s.p < scopey[t].s.x) {
+							scopex[r].s.position < scopey[t].s.content) {
 							eleOut.scopes.push_back(Scope(scopex[r].tid, scopex[r].m,
-														  scopex[r].s.x, scopey[t].s));
+														  scopex[r].s.content, scopey[t].s));
 						}
 					}
 				if (eleOut.check())
