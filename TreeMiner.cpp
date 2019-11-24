725770fc13f422e22d4b66f6c03a533fa0896ef4
gqqnbig <gqqnb2005@gmail.com>
2019-11-23 19:08:25 -0800

    加入更多输出，并可以输出为latex格式

--- TreeMiner.cpp
+++ TreeMiner.cpp
@@ -3,12 +3,11 @@
 #include <cstring>
 #include <vector>
 
-#include <string>
 #include <iterator>
 #include <sstream>
+#include <string>
 
-#define emptySet "[]"
-#define joinOperator "join"
+//#define LatexOutput
 
 //#define debug
 using namespace std;
@@ -16,7 +15,24 @@ typedef vector<int> vi;
 const int N = 100000 + 10;
 const int MinSup = 2;
 
-string vectorToString(vector<int> &m, char *empty) {
+#ifndef LatexOutput
+const bool useLatex = false;
+
+const char* emptySet = "[]";
+const char* joinOperator = "join";
+const char* scopeJoinOperator = "join";
+const char* scopeListSymbol = "L";
+#else
+const bool useLatex = true;
+
+const char* emptySet = "\\varnothing";
+const char* joinOperator = "\\otimes";
+const char* scopeJoinOperator = "\\cap_\\otimes";
+const char* scopeListSymbol = "\\mathcal{L}";
+#endif
+
+
+string vectorToString(vector<int>& m, const char* empty) {
 	if (!m.empty()) {
 		std::ostringstream oss;
 		// Convert all but the last element to avoid a trailing ","
@@ -30,113 +46,122 @@ string vectorToString(vector<int> &m, char *empty) {
 }
 
 struct Pair {
-  int content, position;
-  Pair() {}
-  Pair(int _x, int _p) {
-	  content = _x;
-	  position = _p;
-  }
+	int content, position;
 
-  string toString() {
-	  return "[" + to_string(content) + "," + to_string(position) + "]";
-  }
+	Pair() {}
 
-  string toLabelString() {
-	  return "[" + string(1, 'A' - 1 + content) + "," + to_string(position) + "]";
-  }
+	Pair(int _x, int _p) {
+		content = _x;
+		position = _p;
+	}
 
+	string toString() {
+		return "(" + to_string(content) + "," + to_string(position) + ")";
+	}
+
+	string toLabelString() {
+		return "(" + string(1, 'A' - 1 + content) + "," + to_string(position) + ")";
+	}
 };
+
 struct Scope {
-  int tid; // tree id
-  vi m;    // match label of the k-1 length prefix
-  Pair s;  // scope of the last item
-  Scope() {}
-  Scope(int _tid, vi _m, int lx, Pair _s) {
-	  tid = _tid;
-	  m = _m;
-	  m.push_back(lx);
-	  s = _s;
-  }
+	int tid; // tree id
+	vi m;    // match label of the k-1 length prefix
+	Pair s;  // scope of the last item
+	Scope() {}
 
-  std::string toString() {
-	  return "(t=" + std::to_string(tid) + ", m=" + vectorToString(m, emptySet) + ", s=" + s.toString() + ")";
-  }
+	Scope(int _tid, vi _m, int lx, Pair _s) {
+		tid = _tid;
+		m = _m;
+		m.push_back(lx);
+		s = _s;
+	}
+
+	std::string toString(bool showArgumentName = true) {
+		if (showArgumentName)
+			return "(t=" + std::to_string(tid) + ", m=" + vectorToString(m, emptySet) +
+						 ", s=" + s.toString() + ")";
+		else
+			return "(" + std::to_string(tid) + ", " + vectorToString(m, emptySet) +
+						 ", " + s.toString() + ")";
+	}
 };
-struct Element {
-  //Possible position
-  Pair label;
-  vector<Scope> scopes;
 
-  //check support
-  bool check() {
+struct Element {
+	// Possible position
+	Pair label;
+	vector<Scope> scopes;
+
+	// check support
+	bool check() {
 		int cnt = 0, checkedTree = -1;
 
 		for (int i = 0; i < scopes.size(); i++) {
 			int tid = scopes[i].tid;
 			if (tid != checkedTree)
 				cnt++, checkedTree = tid;
-	  }
-	  return cnt >= MinSup;
-  }
+		}
+		return cnt >= MinSup;
+	}
 } element[N];
+
 struct Subtree {
-  vi prefix;
-  vector<Element> elements;
+	vi prefix;
+	vector<Element> elements;
 
-  string prefixToString() {
-	  string s;
-	  char buff[50];
-	  for (int i = 0; i < prefix.size(); i++) {
-		  if (prefix[i] == -1)
-			  sprintf(buff, "%d ", prefix[i]);
-		  else
-			  printf(buff, "%c ", 'A' - 1 + prefix[i]);
+	string prefixToString() {
+		string s;
+		for (int i = 0; i < prefix.size(); i++) {
+			char buff[50];
+			if (prefix[i] == -1)
+				sprintf(buff, "%d ", prefix[i]);
+			else
+				sprintf(buff, "%c ", 'A' - 1 + prefix[i]);
 
-		  s = s + buff;
-	  }
-	  return s;
-  }
+			s = s + buff;
+		}
+		return s;
+	}
 
-  //print the tree
-  void output() {
+	// print the tree
+	void output() {
 
-	  if (elements.size() == 0)
-		  return;
-	  printf("prefix (class template): ");
-	  if (prefix.size() == 0)
-		  printf("empty");
-	  else {
-		  for (int i = 0; i < prefix.size(); i++)
-			  if (prefix[i] == -1)
-				  printf("%d ", prefix[i]);
-			  else
-				  printf("%c ", 'A' - 1 + prefix[i]);
-	  }
-	  puts("");
-	  for (int i = 0; i < elements.size(); i++) {
-		  // puts("----------");
-		  printf("New node: ");
+		if (elements.size() == 0)
+			return;
+		printf("prefix (class template): ");
+		if (prefix.size() == 0)
+			printf("empty");
+		else {
+			printf(prefixToString().c_str());
+		}
+		puts("");
+		for (int i = 0; i < elements.size(); i++) {
+			// puts("----------");
+			printf("New node: ");
 
-		  printf("%1$c %2$d (add node %1$c to DFS position %2$d)\n", 'A' + elements[i].label.content - 1, elements[i].label.position);
+			printf("%1$c %2$d (add node %1$c to DFS position %2$d)\n",
+						 'A' + elements[i].label.content - 1, elements[i].label.position);
 #ifdef debug
-		  vector<Scope> &scopes = elements[i].scopes;
-		  for (int j = 0; j < scopes.size(); j++) {
-			  printf("%d,", scopes[j].tid);
-			  for (int k = 0; k < scopes[j].m.size(); k++)
-				  printf("%d%c", scopes[j].m[k],
-						 k + 1 == scopes[j].m.size() ? ',' : ' ');
-			  printf("[%d,%d]\n", scopes[j].s.content, scopes[j].s.position);
-		  }
+			vector<Scope>& scopes = elements[i].scopes;
+			for (int j = 0; j < scopes.size(); j++) {
+				printf("%d,", scopes[j].tid);
+				for (int k = 0; k < scopes[j].m.size(); k++)
+					printf("%d%c", scopes[j].m[k],
+								 k + 1 == scopes[j].m.size() ? ',' : ' ');
+				printf("[%d,%d]\n", scopes[j].s.content, scopes[j].s.position);
+			}
 #endif
-	  }
-  }
+		}
+	}
 };
+
 void pt(vi v) {
 	for (int i = 0; i < v.size(); i++)
 		printf("%d ", v[i]);
 	puts("");
 }
-void work(const vi &v, int tid) {
+
+void work(const vi& v, int tid) {
 #ifdef debug
 	for (int i = 0; i < v.size(); i++) {
 		if (v[i] == -1)
@@ -168,7 +193,8 @@ void work(const vi &v, int tid) {
 		++lx;
 	}
 }
-void Prefix_Generate(const vi &pre1, const Pair &label, vi &pre2) {
+
+void Prefix_Generate(const vi& pre1, const Pair& label, vi& pre2) {
 	pre2 = pre1;
 	for (int i = 0, fa = 0; i < pre1.size(); i++) {
 		if (pre1[i] == -1)
@@ -188,42 +214,52 @@ void Prefix_Generate(const vi &pre1, const Pair &label, vi &pre2) {
 	}
 	pre2.push_back(label.content);
 }
+
 void TMdfs(Subtree P) {
 	P.output();
 	for (int i = 0; i < P.elements.size(); i++) {
 		Subtree PP;
 		Pair px = P.elements[i].label;
 		Prefix_Generate(P.prefix, px, PP.prefix);
-		vector<Scope> &scopex = P.elements[i].scopes;
+
+		printf("Try prefix %s.\n", PP.prefixToString().c_str());
+		vector<Scope>& scopex = P.elements[i].scopes;
 		for (int j = 0; j < P.elements.size(); j++) {
 			Pair py = P.elements[j].label;
-			vector<Scope> &scopey = P.elements[j].scopes;
+			vector<Scope>& scopey = P.elements[j].scopes;
 			// In-scope Test
 			if (px.position == py.position) {
 				Element eleIn;
 				eleIn.label = Pair(py.content, py.position + 1); //(y,j+1)
-				printf("In-scope join %s and %s, get %s:\n", px.toLabelString().c_str(), py.toLabelString().c_str(), eleIn.label.toLabelString().c_str());
+				printf((string("In-scope: ") + (useLatex ? "$" : "") + scopeListSymbol + "(%s) %s " + scopeListSymbol + "(%s)=" + scopeListSymbol + "(%s)" + (useLatex ? "$" : "") + ":\n").c_str(),
+							 px.toLabelString().c_str(),
+							 scopeJoinOperator, py.toLabelString().c_str(),
+							 eleIn.label.toLabelString().c_str());
 				for (int r = 0; r < scopex.size(); r++)
 					for (int t = 0; t < scopey.size(); t++) {
-						//skip same element
+						// skip same element
 						if ((&scopex[r]) == (&scopey[t]))
 							continue;
 						bool mtest = scopex[r].m == scopey[t].m;
 						if (scopex[r].tid == scopey[t].tid && mtest &&
-							scopex[r].s.content <= scopey[t].s.content &&
-							scopex[r].s.position >= scopey[t].s.position) {
+								scopex[r].s.content <= scopey[t].s.content &&
+								scopex[r].s.position >= scopey[t].s.position) {
 
-							Scope s = Scope(scopex[r].tid, scopex[r].m,
-											scopex[r].s.content, scopey[t].s);
-							printf("%s %s %s=%s.\n", scopex[r].toString().c_str(), joinOperator, scopey[t].toString().c_str(), s.toString().c_str());
+							Scope s = Scope(scopex[r].tid, scopex[r].m, scopex[r].s.content,
+															scopey[t].s);
+							printf((string("") + (useLatex ? "$" : "") + "%s %s %s=%s" + (useLatex ? "$" : "") + "\n").c_str(), scopex[r].toString().c_str(),
+										 joinOperator, scopey[t].toString().c_str(),
+										 s.toString(false).c_str());
 							eleIn.scopes.push_back(s);
 						}
 					}
-				if (eleIn.check()) {
+				if (eleIn.scopes.size() == 0)
+					printf("but %s(%s) has nothing.\n", scopeListSymbol, eleIn.label.toLabelString().c_str());
+				else if (eleIn.check()) {
 					PP.elements.push_back(eleIn);
-					puts("The pattern is frequent.");
+					puts("The pattern is frequent based on the scope list.");
 				} else {
-					puts("The pattern is not frequent.");
+					puts("The pattern is not frequent on the scope list.");
 				}
 			}
 			// Out-scope Test
@@ -231,21 +267,36 @@ void TMdfs(Subtree P) {
 			if (py.position != -1) {
 				Element eleOut;
 				eleOut.label = Pair(py.content, py.position); //(y,j)
+				printf((string("out-scope: ") + (useLatex ? "$" : "") + scopeListSymbol + "(%s) %s " + scopeListSymbol + "(%s)=" + scopeListSymbol + "(%s)" + (useLatex ? "$" : "") + ":\n").c_str(),
+							 px.toLabelString().c_str(),
+							 scopeJoinOperator, py.toLabelString().c_str(),
+							 eleOut.label.toLabelString().c_str());
 				for (int r = 0; r < scopex.size(); r++)
 					for (int t = 0; t < scopey.size(); t++) {
 						if (scopex[r].tid == scopey[t].tid && scopex[r].m == scopey[t].m &&
-							scopex[r].s.position < scopey[t].s.content) {
-							eleOut.scopes.push_back(Scope(scopex[r].tid, scopex[r].m,
-														  scopex[r].s.content, scopey[t].s));
+								scopex[r].s.position < scopey[t].s.content) {
+							Scope s = Scope(scopex[r].tid, scopex[r].m,
+																		 scopex[r].s.content, scopey[t].s);
+							printf((string("") + (useLatex ? "$" : "") + "%s %s %s=%s" + (useLatex ? "$" : "") + "\n").c_str(), scopex[r].toString().c_str(),
+										 joinOperator, scopey[t].toString().c_str(),
+										 s.toString(false).c_str());
+							eleOut.scopes.push_back(s);
 						}
 					}
-				if (eleOut.check())
+				if (eleOut.scopes.size() == 0)
+					printf("but %s(%s) has nothing.\n", scopeListSymbol, eleOut.label.toLabelString().c_str());
+				else if (eleOut.check()) {
+					puts("The pattern is frequent based on the scope list.");
 					PP.elements.push_back(eleOut);
+				} else
+					puts("The pattern is not frequent on the scope list.");
+
 			}
 		}
 		TMdfs(PP);
 	}
 }
+
 void F1() {
 	int i, tot = 0, treeId = 1;
 	vi v;
@@ -263,6 +314,7 @@ void F1() {
 	}
 	TMdfs(P1);
 }
+
 int main() {
 	// freopen("test.txt","r",stdin);
 	freopen("../F5.data", "r", stdin);
